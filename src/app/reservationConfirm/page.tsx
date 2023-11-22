236e261a235e643437142d6a1de32eaf39653bd5
jinjoo-jung <wjdwlswn23@naver.com>
2023-11-23 01:54:33 +0900

    Feat: add reservation Page

---
+++ src/app/reservationConfirm/page.tsx
@@ -0,0 +1,12 @@
+import ReservationConfirm from '@/components/ReservationConfirm';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <ReservationConfirm />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/ReservationConfirm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReservationConfirm = () => {
+  return (
+    <div>
+      <div>최근 6개월</div>
+    </div>
+  );
+};
+
+export default ReservationConfirm;
