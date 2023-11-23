a7b6fe626e81a7e4db65259a5dc9c2ab14fdf485
jinjoo-jung <wjdwlswn23@naver.com>
2023-11-23 17:07:11 +0900

    Feat:add ReservationConfirm.tsx

--- src/app/reservationConfirm/page.tsx
+++ src/app/reservationConfirm/page.tsx
@@ -1,12 +1,67 @@
 import ReservationConfirm from '@/components/ReservationConfirm';
 import React from 'react';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 const page = () => {
   return (
-    <div>
-      <ReservationConfirm />
+    <div className='bg-white'>
+      <p className='text-center text-3xl'>국내 여행 예약 내역</p>
+      <div className='flex'>
+        <div className='tex-2xl'>최근 6개월</div>
+        <MdOutlineKeyboardArrowDown />
+      </div>
+      <ReservationConfirm
+        productName={reservationData.productName}
+        roomName={reservationData.roomName}
+        imageUrl={reservationData.imageUrl}
+        visitType={reservationData.visitType}
+        checkInDate={reservationData.checkInDate}
+        checkOutDate={reservationData.checkOutDate}
+        checkInTime={reservationData.checkInTime}
+        checkOutTime={reservationData.checkOutTime}
+        baseGuestCount={reservationData.baseGuestCount}
+        maxGuestCount={reservationData.maxGuestCount}
+      />
+      <ReservationConfirm
+        productName={reservationData.productName}
+        roomName={reservationData.roomName}
+        imageUrl={reservationData.imageUrl}
+        visitType={reservationData.visitType}
+        checkInDate={reservationData.checkInDate}
+        checkOutDate={reservationData.checkOutDate}
+        checkInTime={reservationData.checkInTime}
+        checkOutTime={reservationData.checkOutTime}
+        baseGuestCount={reservationData.baseGuestCount}
+        maxGuestCount={reservationData.maxGuestCount}
+      />
+      <ReservationConfirm
+        productName={reservationData.productName}
+        roomName={reservationData.roomName}
+        imageUrl={reservationData.imageUrl}
+        visitType={reservationData.visitType}
+        checkInDate={reservationData.checkInDate}
+        checkOutDate={reservationData.checkOutDate}
+        checkInTime={reservationData.checkInTime}
+        checkOutTime={reservationData.checkOutTime}
+        baseGuestCount={reservationData.baseGuestCount}
+        maxGuestCount={reservationData.maxGuestCount}
+      />
     </div>
   );
 };
 
 export default page;
+
+// 예약 내역 데이터
+const reservationData = {
+  productName: '몬드리안 서울 이태원',
+  roomName: '이태원 몬드리안 썸머바캉스 조식2인',
+  imageUrl: '/images/roomImg.png',
+  visitType: '도보',
+  checkInTime: '15:00',
+  checkOutTime: '12:00',
+  baseGuestCount: 2,
+  maxGuestCount: 4,
+  checkInDate: '2023.11.20',
+  checkOutDate: '2023.11.21',
+};
--- src/components/ReservationConfirm.tsx
+++ src/components/ReservationConfirm.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import Image from 'next/image';
+import { IoIosArrowForward } from 'react-icons/io';
 
-const ReservationConfirm = () => {
+const ReservationConfirm = ({
+  productName,
+  roomName,
+  imageUrl,
+  visitType,
+  checkInDate,
+  checkOutDate,
+  checkInTime,
+  checkOutTime,
+  baseGuestCount,
+  maxGuestCount,
+}: ProductProps) => {
   return (
-    <div>
-      <div>최근 6개월</div>
+    <div className='bg-white'>
+      <div className='flex'>
+        <div className='text-base'>숙소 예약번호: 123891734892748</div>
+        <div className='flex'>
+          <div className='text-base'>상세보기</div>
+          <IoIosArrowForward />
+        </div>
+      </div>
+      <div className='text-xs'>예약완료</div>
+      <div className='text-xl'>{productName}</div>
+      <div className='text-xl'>{roomName}</div>
+      <div className='flex'>
+        <Image width={50} height={50} src={imageUrl} alt='room img' />
+        <div>
+          <div className='text-base'>{visitType}방문</div>
+          <div className='text-base'>
+            {checkInDate} ~ {checkOutDate}
+          </div>
+          <div className='text-base'>
+            체크인 {checkInTime} 체크아웃 {checkOutTime}
+          </div>
+          <div className='text-base'>
+            기준 {baseGuestCount}명 / 최대 {maxGuestCount}명
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default ReservationConfirm;
+
+interface ProductProps {
+  productName: string;
+  roomName: string;
+  imageUrl: string;
+  visitType: string;
+  checkInTime: string;
+  checkOutTime: string;
+  baseGuestCount: number;
+  maxGuestCount: number;
+  checkInDate: string;
+  checkOutDate: string;
+}
