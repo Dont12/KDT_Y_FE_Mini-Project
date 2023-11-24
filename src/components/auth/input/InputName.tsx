de15e092f04b4f77607b622d0a9c2a9593121024
NamgungJongMin <jmnamgung@gmail.com>
2023-11-24 20:30:26 +0900

    Feat: add signin & signup markup and styling

---
+++ src/components/auth/input/InputName.tsx
@@ -0,0 +1,37 @@
+import { ErrorMsg, ValidationIcon } from '@components/auth';
+import { memo } from 'react';
+
+interface InputType {
+  value: string;
+  validationPass: boolean;
+}
+
+interface NameProps {
+  name: InputType;
+  handleName: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const InputName = memo(({ name, handleName }: NameProps) => (
+  <div className='relative mt-3'>
+    <label htmlFor='user' className='text-base leading-10'>
+      이름*
+    </label>
+
+    <input
+      type='text'
+      name='user'
+      id='user'
+      value={name.value}
+      placeholder='이름을 입력해주세요.'
+      onChange={handleName}
+      required
+      autoComplete='off'
+      className='border-lightGray top-10 h-14 w-full rounded-[20px] border-2 p-4 text-base text-black'
+    />
+
+    <ValidationIcon input={name} />
+    <ErrorMsg target='name' input={name} />
+  </div>
+));
+
+export default InputName;
