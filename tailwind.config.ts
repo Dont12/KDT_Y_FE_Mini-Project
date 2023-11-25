0a446be31fe28bc7076dd881455593f413901830
jisu Seo <jseo9732@gmail.com>
2023-11-26 00:44:09 +0900

    Conf: temporarily add color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ export default {
         lightGray: '#D9D9D9',
         mediumGray: '#B3B3B3',
         darkGray: '#595757',
+        background: '#F2F2F2F2',
         black: '#000000',
         red: '#FF0000',
         blue: '#0075FF',
