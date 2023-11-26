6c82fb27de094c549dbbb9c6a7c86052564e2fb6
moonyah <dltk456@naver.com>
2023-11-26 16:31:54 +0900

    Fixed: remove IconWithLabel

--- src/components/main/IconList.tsx
+++ src/components/main/IconList.tsx
@@ -1,24 +1,29 @@
 // components/main/IconList.tsx
+import Image from 'next/image';
 import React from 'react';
 
-import IconWithLabel from './IconWithLabel';
+interface IconProps {
+  icon: string;
+  label: string;
+}
 
 interface IconListProps {
   icons: { icon: string; label: string }[];
 }
 
-const IconList = ({ icons }: IconListProps) => {
-  return (
-    <div className='icon-container grid grid-cols-5 justify-items-center gap-10 pb-10 pl-12 pr-12'>
-      {icons.map((iconData, index) => (
-        <IconWithLabel
-          key={index}
-          icon={iconData.icon}
-          label={iconData.label}
-        />
-      ))}
-    </div>
-  );
-};
+const Icon = ({ icon, label }: IconProps) => (
+  <div className='flex flex-col items-center'>
+    <Image src={icon} alt={label} width={46} height={46} />
+    <span className='mt-2 text-xs'>{label}</span>
+  </div>
+);
+
+const IconList = ({ icons }: IconListProps) => (
+  <div className='icon-container grid grid-cols-5 justify-items-center gap-10 pb-10 pl-12 pr-12'>
+    {icons.map((iconData, index) => (
+      <Icon key={index} {...iconData} />
+    ))}
+  </div>
+);
 
 export default IconList;
