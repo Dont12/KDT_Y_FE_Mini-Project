91ba2222d7ff03b2b954afa29837d00688626106
jisu Seo <jseo9732@gmail.com>
2023-11-28 15:46:10 +0900

    Style: remove JSX.Element

--- src/components/common/footer/FooterButton.tsx
+++ src/components/common/footer/FooterButton.tsx
@@ -3,7 +3,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-const FooterButton = ({ href, children }: Props): JSX.Element => {
+const FooterButton = ({ href, children }: Props) => {
   return (
     <li className='footer-button px-[0.3125rem] py-1'>
       <a href={href} target='_blank'>
