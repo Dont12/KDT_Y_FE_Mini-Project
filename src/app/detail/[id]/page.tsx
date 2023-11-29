5be77d87bc0d907f779488bf20fa91d173a60298
Jong Min Namgung <jmnamgung@gmail.com>
2023-11-29 16:09:20 +0900

    Revert "Revert "Feat : connect API with detail page""

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,202 @@
+import Image from 'next/image';
+import React from 'react';
+
+import {
+  Carousel,
+  CartButton,
+  DatePicker,
+  Header,
+  HeaderNav,
+  KakaoMap,
+  PersonInput,
+  ReservationButton,
+  Rules,
+} from '@/components/detail/index';
+
+import { DetailResponse, Room } from '@/@types/detail.types';
+import detailInfoRequest from '@/api/detailInfoRequest';
+import { calculateTotalCost } from '@/utils/calculatePerNightCost';
+
+const today = new Date();
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+const formatDate = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+const Detail = async ({
+  params,
+  searchParams = { checkInDate: '', checkOutDate: '', guest: '' },
+}: {
+  params: { id: string };
+  searchParams: { checkInDate: string; checkOutDate: string; guest: string };
+}) => {
+  const defaultCheckInDate = searchParams.checkInDate || formatDate(today);
+  const defaultCheckOutDate = searchParams.checkOutDate || formatDate(tomorrow);
+  const defaultPerson = searchParams.guest || '1';
+
+  const details: DetailResponse = await detailInfoRequest.getDetail({
+    id: params.id,
+    checkIn: defaultCheckInDate,
+    checkOut: defaultCheckOutDate,
+  });
+
+  return (
+    <>
+      <Header>
+        <HeaderNav showBack showCart showHome>
+          {details.data.name}
+        </HeaderNav>
+      </Header>
+      <main>
+        <div className='mt-12 bg-white'>
+          <div>
+            {/* 이미지가 배열로 들어오면 [] 벗기기 */}
+            <Carousel images={[details.data.imageUrl]} />
+          </div>
+          <div className='flex flex-col gap-4 p-5'>
+            <div className='border-mediumGray border-b border-solid pb-3'>
+              <h3 className='text-[24px]'>{details.data.name}</h3>
+            </div>
+            <div className='border-mediumGray border-b border-solid pb-3'>
+              <div id='map' className='h-60 w-full'>
+                <KakaoMap
+                  longitude={details.data.longitude}
+                  latitude={details.data.latitude}
+                />
+              </div>
+              <p>{details.data.address}</p>
+              <p>
+                체크인 {details.data.rooms[0].checkInTime} - 체크아웃
+                {details.data.rooms[0].checkOutTime}
+              </p>
+            </div>
+            <div className='border-mediumGray flex justify-evenly border-b border-solid pb-3 '>
+              <div className='flex flex-col'>
+                <label htmlFor='check'>체크인-체크아웃</label>
+                <DatePicker
+                  roomId={params.id}
+                  checkIn={defaultCheckInDate}
+                  checkOut={defaultCheckOutDate}
+                  guest={defaultPerson}
+                />
+              </div>
+              <div>
+                <PersonInput
+                  roomId={params.id}
+                  checkIn={defaultCheckInDate}
+                  checkOut={defaultCheckOutDate}
+                  guest={defaultPerson}
+                />
+              </div>
+            </div>
+            <div className='flex flex-col gap-10'>
+              {details.data.rooms.map((room: Room, index: number) => (
+                <div
+                  key={index}
+                  className='border-mediumGray flex flex-col gap-5 rounded border border-solid p-5'
+                >
+                  <div className='flex justify-between'>
+                    <div className='mr-5'>
+                      <Image
+                        src={room.imageUrl}
+                        width={150}
+                        height={150}
+                        alt={`Room ${index + 1}`}
+                        className='h-full w-48 object-cover'
+                      />
+                    </div>
+                    <div className='flex grow flex-col gap-3 pt-3'>
+                      <div>
+                        <p className='font-bold'>{room.name}</p>
+                        <p>최대 인원: {room.maxGuestCount}</p>
+                        <p>정원: {room.basicGuestCount}</p>
+                        <p>
+                          옵션:
+                          {room.roomBathFacility === 'Y' && '욕조, '}
+                          {room.roomBath === 'Y' && '욕실, '}
+                          {room.roomHomeTheater === 'Y' && '홈시어터, '}
+                          {room.roomAircondition === 'Y' && '에어컨, '}
+                          {room.roomTv === 'Y' && 'TV, '}
+                          {room.roomPc === 'Y' && 'PC, '}
+                          {room.roomCable === 'Y' && '멀티탭, '}
+                          {room.roomInternet === 'Y' && 'WIFI, '}
+                          {room.roomRefrigerator === 'Y' && '냉장고, '}
+                          {room.roomToiletries === 'Y' && '욕실용품, '}
+                          {room.roomSofa === 'Y' && '소파, '}
+                          {room.roomCook === 'Y' && '주방, '}
+                          {room.roomTable === 'Y' && '식탁, '}
+                          {room.roomHairdryer === 'Y' && '헤어드라이기'}
+                        </p>
+                      </div>
+                      <div className='flex items-end justify-end'>
+                        <p className='text-baseline text-[20px] font-bold leading-4'>
+                          남은 객실: {room.stock}
+                        </p>
+                      </div>
+                      <div className='flex flex-row items-end justify-end gap-1'>
+                        <p className='text-gray1 text-[16px] font-bold'>
+                          1박당/
+                        </p>
+                        <p className='m-0 text-[26px] font-bold'>
+                          {new Intl.NumberFormat().format(room.price)}원
+                        </p>
+                      </div>
+                      <div className='flex flex-row items-end justify-end'>
+                        <p>
+                          {'총 '}
+                          {new Intl.NumberFormat().format(
+                            calculateTotalCost({
+                              checkInDate: defaultCheckInDate,
+                              checkOutDate: defaultCheckOutDate,
+                              numberOfGuests: defaultPerson,
+                              maxRoomCapacity: room.maxGuestCount,
+                              pricePerNight: room.price,
+                            })
+                          )}
+                          원
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex flex-row'>
+                    <CartButton
+                      roomId={room.id}
+                      checkInDate={defaultCheckInDate}
+                      checkOutDate={defaultCheckOutDate}
+                      guestCount={defaultPerson}
+                    />
+                    <ReservationButton
+                      productId={params.id}
+                      roomId={room.id}
+                      checkInDate={defaultCheckInDate}
+                      checkInTime={room.checkInTime}
+                      checkOutDate={defaultCheckOutDate}
+                      checkOutTime={room.checkOutTime}
+                      guestCount={defaultPerson}
+                      price={room.price}
+                      stock={room.stock}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className='border-mediumGray border-b border-solid py-8'>
+              <Rules />
+            </div>
+            <div className='py-8'>
+              <p className='text-[18px] font-bold'>숙소소개</p>
+              <p>{details.data.description}</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Detail;
