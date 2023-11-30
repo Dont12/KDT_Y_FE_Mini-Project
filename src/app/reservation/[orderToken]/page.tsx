55182fd34605acb2f084eae1135b16f0b95dac09
HOOOO98 <loading2us@gmail.com>
2023-11-30 17:24:36 +0900

    Feat: restore API error

--- src/app/reservation/[orderToken]/page.tsx
+++ src/app/reservation/[orderToken]/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+
+import { Header, HeaderNav } from '@/components/common/header';
 import {
   CheckBoxGroup,
   ReservationItem,
   UserInformation,
 } from '@/components/reservation';
 
-import Header from '@/components/common/Header';
-import HeaderNav from '@/components/common/HeaderNav';
-import React, { useEffect, useState } from 'react';
 import orderRequest from '@/api/orderRequest';
-import { useRouter } from 'next/navigation';
-import Modal from 'react-modal';
 
 const Divider = () => <div className='border-lightGray border-b  px-8 '></div>;
 
--- src/components/detail/DateRagePicker.tsx
+++ src/components/detail/DateRagePicker.tsx
@@ -3,8 +3,7 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { DateRangePicker } from 'rsuite';
 
-import 'rsuite/dist/rsuite.min.css';
-
+// import 'rsuite/dist/rsuite.min.css';
 import { DatePickerProps } from '@/@types/detail.types';
 
 const today = new Date();
