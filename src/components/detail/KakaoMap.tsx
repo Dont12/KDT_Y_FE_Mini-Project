da097b0b911bb4ed260d5d954d12757a8db55702
HOOOO98 <loading2us@gmail.com>
2023-12-04 14:09:10 +0900

    Fix: fixed to match the kakomap SDK format

--- src/components/detail/KakaoMap.tsx
+++ src/components/detail/KakaoMap.tsx
@@ -1,11 +1,8 @@
 'use client';
 
-import Script from 'next/script';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk';
 
-const JSKey = process.env.NEXT_PUBLIC_KAKAOMAP_JSKEY;
-
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${JSKey}&autoload=false`;
+const JSKey = process.env.NEXT_PUBLIC_KAKAOMAP_JSKEY || '';
 
 const KakaoMap = ({
   longitude,
@@ -13,19 +10,31 @@ const KakaoMap = ({
 }: {
   longitude: string;
   latitude: string;
-}) => (
-  <>
-    <Script src={KAKAO_SDK_URL} strategy='beforeInteractive' />
-    <Map
-      center={{ lat: parseFloat(latitude), lng: parseFloat(longitude) }}
-      style={{ width: '100%', height: '100%' }}
-      maxLevel={3}
-    >
-      <MapMarker
-        position={{ lat: parseFloat(latitude), lng: parseFloat(longitude) }}
-      ></MapMarker>
-    </Map>
-  </>
-);
+}) => {
+  const [loading, error] = useKakaoLoader({
+    appkey: JSKey,
+  });
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {!loading && !error && (
+        <Map
+          center={{ lat: parseFloat(latitude), lng: parseFloat(longitude) }}
+          style={{ width: '100%', height: '100%' }}
+          maxLevel={3}
+        >
+          <MapMarker
+            position={{
+              lat: parseFloat(latitude),
+              lng: parseFloat(longitude),
+            }}
+          ></MapMarker>
+        </Map>
+      )}
+    </>
+  );
+};
 
 export default KakaoMap;
